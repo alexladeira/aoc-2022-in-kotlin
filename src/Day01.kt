58a76d97542e5ba9184fd18bcfde2efd8746b94a
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-12-01 21:29:18 -0300

    AOC - day 01 - part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,16 @@
 fun main() {
     val testInput = getMaxInput("Day01_test")
-    check(testInput == 24000)
+    check(45000 == testInput)
     println(getMaxInput("Day01"))
 }
 
-private fun getMaxInput(fileName: String) = lists(fileName).maxOfOrNull { it.sum() }
+private fun getMaxInput(fileName: String) = lists(fileName).map { it.sum() }.sortedDescending().take(3).sum();
 
-private fun lists(fileName: String) = readInput(fileName).foldIndexed(mutableListOf<MutableList<Int>>()) { index, acc, s ->
-    when {
-        index == 0 || s.isBlank() -> acc.add(ArrayList())
-        else -> acc.last().add(s.toInt())
+private fun lists(fileName: String) =
+    readInput(fileName).foldIndexed(mutableListOf<MutableList<Int>>()) { index, acc, s ->
+        when {
+            index == 0 || s.isBlank() -> acc.add(ArrayList())
+            else -> acc.last().add(s.toInt())
+        }
+        acc
     }
-    acc
-}
