b3a76532c71e52a689f0780dd94c089c3ac213d9
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-12-01 21:20:43 -0300

    AOC - day 01 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,15 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val testInput = getMaxInput("Day01_test")
+    check(testInput == 24000)
+    println(getMaxInput("Day01"))
+}
+
+private fun getMaxInput(fileName: String) = lists(fileName).maxOfOrNull { it.sum() }
+
+private fun lists(fileName: String) = readInput(fileName).foldIndexed(mutableListOf<MutableList<Int>>()) { index, acc, s ->
+    when {
+        index == 0 || s.isBlank() -> acc.add(ArrayList())
+        else -> acc.last().add(s.toInt())
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    acc
 }
