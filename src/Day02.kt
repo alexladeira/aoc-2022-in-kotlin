71966b242bda19a62d75ec241143895e3395361c
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-12-04 02:52:10 -0300

    AOC - day 02 - part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,7 +1,9 @@
 fun main() {
     val finalValues = mapOf("X" to 1, "Y" to 2, "Z" to 3)
     val sum = readInput("Day02").fold(0) { acc, s ->
-        val values = s.split(" ")
+        val values = s.split(" ").toMutableList()
+        values[1] = updateSecondValue(values)
+
         when {
             "A" == values[0] && "Z" == values[1] || "C" == values[0] && "Y" == values[1] || "B" == values[0] && "X" == values[1] -> acc + finalValues[values[1]]!!
             "A" == values[0] && "X" == values[1] || "C" == values[0] && "Z" == values[1] || "B" == values[0] && "Y" == values[1] -> acc + finalValues[values[1]]!! + 3
@@ -9,4 +11,17 @@ fun main() {
         }
     }
     print(sum)
+}
+
+fun updateSecondValue(values: List<String>): String {
+    return if (values[1] == "X") {
+        val lose = mapOf("A" to "Z", "B" to "X", "C" to "Y")
+        lose[values[0]]!!
+    } else if (values[1] == "Y") {
+        val draw = mapOf("A" to "X", "B" to "Y", "C" to "Z")
+        draw[values[0]]!!
+    } else {
+        val win = mapOf("A" to "Y", "B" to "Z", "C" to "X")
+        win[values[0]]!!
+    }
 }
