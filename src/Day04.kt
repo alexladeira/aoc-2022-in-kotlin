facafc2d92de2ad2b6264610be4159c8135babcb
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-12-04 23:58:23 -0300

    AOC - day 04 - part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,8 +1,17 @@
 import kotlin.streams.toList
 
 fun main() {
-    val result = part1()
+    val result = part2()
     println(result)
+}
+
+private fun part2() = readInput("Day04").fold(0) { acc, s ->
+    val values = s.replace("-", ",").split(",").stream().mapToInt { it.toInt() }.toList()
+    if (values[1] >= values[2] && values[0] <= values[3]) {
+        acc + 1
+    } else {
+        acc
+    }
 }
 
 private fun part1() = readInput("Day04").fold(0) { acc, s ->
